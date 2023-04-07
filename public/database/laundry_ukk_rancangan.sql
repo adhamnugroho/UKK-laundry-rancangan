c96038d9dfac4805f4713cb268a8a1b911c7508b
Adham Nugroho <76804759+adhamnugroho@users.noreply.github.com>
2023-04-07 15:04:19 +0700

    add database

---
+++ public/database/laundry_ukk_rancangan.sql
@@ -0,0 +1,446 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 07 Apr 2023 pada 10.03
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `laundry_ukk_rancangan`
+--
+
+DELIMITER $$
+--
+-- Prosedur
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getTotalHarga` (IN `quantity` DOUBLE, IN `id_paket` INT(11), IN `diskon` DOUBLE, IN `id_transaksi` INT(11))   BEGIN
+
+	DECLARE totalHarga int(11);
+    DECLARE hargaPaket int(11);
+    DECLARE hargaDiskon int(11);
+	
+    SELECT harga INTO hargaPaket FROM tb_paket WHERE id = id_paket;
+    
+    IF(diskon != 0) THEN
+		SET hargaDiskon = (quantity * hargaPaket) * diskon;
+        SET totalHarga = (hargaPaket * qty) - hargaDiskon;
+    ELSE
+    	SET totalHarga = quantity * hargaPaket;
+    END IF;
+
+	UPDATE tb_transaksi set total_harga = totalHarga WHERE id = id_transaksi;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(40, '2014_10_12_000000_create_users_table', 1),
+(45, '2023_03_11_054142_create_outlets_table', 1),
+(109, '2014_10_12_100000_create_password_reset_tokens_table', 2),
+(110, '2019_08_19_000000_create_failed_jobs_table', 2),
+(111, '2019_12_14_000001_create_personal_access_tokens_table', 2),
+(112, '2019_12_14_200001_create_outlets_table', 2),
+(113, '2019_12_14_300001_create_users_table', 2),
+(114, '2023_03_11_041102_create_members_table', 2),
+(115, '2023_03_11_060659_create_pakets_table', 2),
+(116, '2023_03_11_073940_create_transaksis_table', 2),
+(117, '2023_03_11_080230_create_detail_transaksis_table', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `password_reset_tokens`
+--
+
+CREATE TABLE `password_reset_tokens` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_detail_transaksi`
+--
+
+CREATE TABLE `tb_detail_transaksi` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_transaksi` bigint(20) UNSIGNED NOT NULL,
+  `id_paket` bigint(20) UNSIGNED NOT NULL,
+  `qty` double NOT NULL,
+  `keterangan` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `tb_detail_transaksi`
+--
+
+INSERT INTO `tb_detail_transaksi` (`id`, `id_transaksi`, `id_paket`, `qty`, `keterangan`, `created_at`, `updated_at`) VALUES
+(5, 5, 1, 2, '-', '2023-03-14 07:08:21', '2023-03-14 07:08:21'),
+(12, 12, 1, 2, '-', '2023-03-14 11:33:05', '2023-03-14 11:33:05'),
+(13, 13, 1, 2, '-', '2023-03-15 07:56:41', '2023-03-15 07:56:41'),
+(14, 14, 1, 2, '-', '2023-03-15 16:19:21', '2023-03-15 16:19:21');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_member`
+--
+
+CREATE TABLE `tb_member` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` text NOT NULL,
+  `jenis_kelamin` enum('L','P') NOT NULL,
+  `tlp` varchar(15) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `tb_member`
+--
+
+INSERT INTO `tb_member` (`id`, `nama`, `alamat`, `jenis_kelamin`, `tlp`, `created_at`, `updated_at`) VALUES
+(1, 'Avan Zacky Mahesa Putra', 'jl. mutiara Kesamben, Jombang, Jawa Timur', 'L', '081293290912', NULL, NULL),
+(2, 'Avrey Ergi Putra Pertama', 'Blotoo', 'L', '081217872376', '2023-03-16 06:38:23', '2023-03-16 06:38:42');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_outlet`
+--
+
+CREATE TABLE `tb_outlet` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` text NOT NULL,
+  `tlp` varchar(15) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `tb_outlet`
+--
+
+INSERT INTO `tb_outlet` (`id`, `nama`, `alamat`, `tlp`, `created_at`, `updated_at`) VALUES
+(1, 'Laris Laundry', 'jl. Mojopahit no 4 Mojokerto', '081293874573', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_paket`
+--
+
+CREATE TABLE `tb_paket` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_outlet` bigint(20) UNSIGNED NOT NULL,
+  `jenis` enum('kiloan','selimut','bed_cover','kaos','lain') NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `tb_paket`
+--
+
+INSERT INTO `tb_paket` (`id`, `id_outlet`, `jenis`, `nama`, `harga`, `created_at`, `updated_at`) VALUES
+(1, 1, 'kiloan', 'Paket cuci basah 1 Kg', 20000, '2023-03-13 08:20:13', '2023-03-13 08:20:13');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_transaksi`
+--
+
+CREATE TABLE `tb_transaksi` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_outlet` bigint(20) UNSIGNED NOT NULL,
+  `kode_invoice` varchar(100) NOT NULL,
+  `id_member` bigint(20) UNSIGNED NOT NULL,
+  `tgl` datetime NOT NULL,
+  `batas_waktu` datetime NOT NULL,
+  `tgl_bayar` datetime NOT NULL,
+  `total_harga` int(11) NOT NULL,
+  `diskon` double NOT NULL,
+  `pajak` int(11) NOT NULL,
+  `status` enum('baru','proses','selesai','diambil') NOT NULL,
+  `dibayar` enum('dibayar','belum_dibayar') NOT NULL,
+  `id_user` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `tb_transaksi`
+--
+
+INSERT INTO `tb_transaksi` (`id`, `id_outlet`, `kode_invoice`, `id_member`, `tgl`, `batas_waktu`, `tgl_bayar`, `total_harga`, `diskon`, `pajak`, `status`, `dibayar`, `id_user`, `created_at`, `updated_at`) VALUES
+(5, 1, 'INV-2023-03-15', 1, '2023-03-15 00:00:00', '2023-03-16 00:00:00', '2023-03-16 00:00:00', 40000, 0, 0, 'diambil', 'dibayar', 2, '2023-03-14 07:08:21', '2023-03-14 11:25:41'),
+(12, 1, 'INV-2023-03-16', 1, '2023-03-16 00:00:00', '2023-03-24 00:00:00', '2023-03-24 00:00:00', 40000, 0, 0, 'diambil', 'dibayar', 2, '2023-03-14 11:33:05', '2023-03-14 16:20:48'),
+(13, 1, 'INV-2023-03-14', 1, '2023-03-14 00:00:00', '2023-03-16 00:00:00', '2023-03-16 00:00:00', 400000, 10, 0, 'baru', 'belum_dibayar', 2, '2023-03-15 07:56:41', '2023-03-15 07:56:41'),
+(14, 1, 'INV-2023-03-17', 1, '2023-03-17 00:00:00', '2023-03-18 00:00:00', '2023-03-18 00:00:00', 4000, 0.1, 0, 'diambil', 'dibayar', 2, '2023-03-15 16:19:21', '2023-03-15 16:20:02');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` text NOT NULL,
+  `id_outlet` bigint(20) UNSIGNED DEFAULT NULL,
+  `role` enum('admin','kasir','owner') NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `tb_user`
+--
+
+INSERT INTO `tb_user` (`id`, `nama`, `username`, `password`, `id_outlet`, `role`, `created_at`, `updated_at`) VALUES
+(1, 'Adham Nugroho', 'adham', '$2y$10$OZPQpLJ5PrWPsV9C4bKjHecieoPMgv8d42yUhmFr8urMvysLG.6eS', 1, 'owner', NULL, NULL),
+(2, 'Aditya Alan', 'alan', '$2y$10$vwaturCjyqnNDXWym5VA2.vd9tOMW2MVI3eCBPiLyvT2JPfz7jDly', 1, 'admin', NULL, NULL),
+(3, 'Adyatma Rifqi', 'adyatma', '$2y$10$XBfzJF9I7beqSkvNPw7TGOPSd39R.nOlCe2HnvwIvf0ewEwrqTdeO', 1, 'kasir', NULL, NULL),
+(4, 'Bagus Indra Pradipa', 'bagus', '$2y$10$/NEVehbZyhwQCdnErxtOPuXRxGvlaD9U6pc.ktjP7Gg6/672RiX86', 1, 'kasir', '2023-03-16 06:43:10', '2023-03-16 06:43:24');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indeks untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indeks untuk tabel `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indeks untuk tabel `tb_detail_transaksi`
+--
+ALTER TABLE `tb_detail_transaksi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tb_detail_transaksi_id_transaksi_foreign` (`id_transaksi`),
+  ADD KEY `tb_detail_transaksi_id_paket_foreign` (`id_paket`);
+
+--
+-- Indeks untuk tabel `tb_member`
+--
+ALTER TABLE `tb_member`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_outlet`
+--
+ALTER TABLE `tb_outlet`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_paket`
+--
+ALTER TABLE `tb_paket`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tb_paket_id_outlet_foreign` (`id_outlet`);
+
+--
+-- Indeks untuk tabel `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tb_transaksi_id_outlet_foreign` (`id_outlet`),
+  ADD KEY `tb_transaksi_id_member_foreign` (`id_member`),
+  ADD KEY `tb_transaksi_id_user_foreign` (`id_user`);
+
+--
+-- Indeks untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tb_user_id_outlet_foreign` (`id_outlet`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=118;
+
+--
+-- AUTO_INCREMENT untuk tabel `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_detail_transaksi`
+--
+ALTER TABLE `tb_detail_transaksi`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_member`
+--
+ALTER TABLE `tb_member`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_outlet`
+--
+ALTER TABLE `tb_outlet`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_paket`
+--
+ALTER TABLE `tb_paket`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `tb_detail_transaksi`
+--
+ALTER TABLE `tb_detail_transaksi`
+  ADD CONSTRAINT `tb_detail_transaksi_id_paket_foreign` FOREIGN KEY (`id_paket`) REFERENCES `tb_paket` (`id`),
+  ADD CONSTRAINT `tb_detail_transaksi_id_transaksi_foreign` FOREIGN KEY (`id_transaksi`) REFERENCES `tb_transaksi` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `tb_paket`
+--
+ALTER TABLE `tb_paket`
+  ADD CONSTRAINT `tb_paket_id_outlet_foreign` FOREIGN KEY (`id_outlet`) REFERENCES `tb_outlet` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  ADD CONSTRAINT `tb_transaksi_id_member_foreign` FOREIGN KEY (`id_member`) REFERENCES `tb_member` (`id`),
+  ADD CONSTRAINT `tb_transaksi_id_outlet_foreign` FOREIGN KEY (`id_outlet`) REFERENCES `tb_outlet` (`id`),
+  ADD CONSTRAINT `tb_transaksi_id_user_foreign` FOREIGN KEY (`id_user`) REFERENCES `tb_user` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD CONSTRAINT `tb_user_id_outlet_foreign` FOREIGN KEY (`id_outlet`) REFERENCES `tb_outlet` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
